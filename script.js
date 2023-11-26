719aa8933f03660de284f016c4859be8d8e5cc92
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-11-26 18:00:12 +0100

    updated square function and  created askForSquares

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+function askForSquares() {
+    const button = document.querySelector(".button");
+
+    button.addEventListener("click", () => {
+        const answer = prompt("Please eneter number of squares per side for the new grid.");
+        if (answer) {
+            deleteSquare();
+            createSquare(parseInt(answer));
+        }
+    })
+}
+
+function deleteSquare() {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+}
 
 function createSquare() {
     const container = document.querySelector(".container");
-    for (let i = 0; i < 16; i++) {
+
+    for (let i = 0; i < 256; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            square.classList.add("hover");
+        });
     }
 }
-createSquare();
+
+function init() {
+    askForSquares();
+    createSquare();
+}
+
+init();
